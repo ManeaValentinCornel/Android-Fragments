9ee6c31a41cf21c91c4349aee616ca343840b282
ManeaValentinCornel <manea.valentin.cornel@gmail.com>
2020-02-27 14:09:36 +0000

    Adding Fragment statically using <fragment> tag

---
+++ F02_AddingFragment_Statically_NonProgrammatically/app/src/main/java/vcmanea/example/f02_addingfragment_statically_nonprogrammatically/MainActivity.java
@@ -0,0 +1,14 @@
+package vcmanea.example.f02_addingfragment_statically_nonprogrammatically;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
